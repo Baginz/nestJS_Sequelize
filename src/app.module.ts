77c70fb00a3fc5366957670fcbcf17e932145dc9
Виталий А. Кузьмичев <vitaly.kuzmichev@arealidea.ru>
2022-05-12 10:57:44 +0300

    addDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestdb',
+      models: [],
+      //создавал таблицы в бд на основании наших моделей
+      autoLoadModels: true,
+    }),
+  ],
   //Контроллеры обрабатывают всю логику за проверкой параметров запроса, запроса, отправки ответов с правильными кодами.
   controllers: [AppController],
   //все что содержит логику и может исп в др. компонентах
