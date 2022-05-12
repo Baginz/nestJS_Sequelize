e58f3474d0ff51aea92883b0b77873d04615fd13
Виталий А. Кузьмичев <vitaly.kuzmichev@arealidea.ru>
2022-05-12 12:21:57 +0300

    addUsers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.model';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
@@ -17,16 +16,16 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       //сам создавал таблицы в бд на основании наших моделей
       autoLoadModels: true,
     }),
     UsersModule,
   ],
   //Контроллеры обрабатывают всю логику за проверкой параметров запроса, запроса, отправки ответов с правильными кодами.
-  controllers: [AppController],
+  controllers: [],
   //все что содержит логику и может исп в др. компонентах
   //Службы содержат запросы к базе данных и возвращающие объекты или метательные ошибки
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-//сервис будем внедрять в контроллер делать иньекцию
-@Injectable()
-export class AppService {
-  getUsers() {
-    return [{ id: 1, name: 'egor' }, { id: 2, name: 'vitaliy' }];
-  }
-}
