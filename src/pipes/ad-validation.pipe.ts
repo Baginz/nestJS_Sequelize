bb411eb79e183900971538b31581e3f3e7dcdc83
Виталий А. Кузьмичев <vitaly.kuzmichev@arealidea.ru>
2022-05-19 15:25:59 +0300

    addPipe

---
+++ src/pipes/ad-validation.pipe.ts
@@ -0,0 +1,19 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class IdValidationPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (metadata.type != 'param') {
+      return value;
+    }
+    if (!value) {
+      throw new BadRequestException('Неверный формат id');
+    }
+    return value;
+  }
+}
